70cba3795c12f126ce752af39879959a10916132
Adrienne Paquin <adriennepaquin@gmail.com>
2021-03-28 23:47:03 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -10,8 +10,7 @@ function callMe() {
 
 function crazy() {
   // fix the code in here:
-  function thisIsCrazy {
-    console.log("hey!!!");
+  console.log("hey!!!");
   }
 
   //thisIsCrazy = function (){
